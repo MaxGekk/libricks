85a86ad1a52c960535fa609b73a5aab2a8a9fa05
Maxim Gekk <maxim.gekk@databricks.com>
2017-12-20 10:42:12 +0100

    Comments for the Common module

--- src/main/scala/Common.scala
+++ src/main/scala/Common.scala
@@ -1,6 +1,17 @@
-
+/**
+  * Interface of REST API endpoint
+  */
 trait Endpoint {
+  /**
+    * Suffix of full path to an endpoint. For instance:
+    * https://shardname.cloud.databricks.com:443/api/2.0/token/create
+    */
   def path: String
 }
 
+/**
+  * The exception is thrown if status code of http response isn't 200
+  * @param statusCode - http status code
+  * @param msg - message entity of error response
+  */
 class RestApiReqException(statusCode: Long, msg: String) extends Exception
