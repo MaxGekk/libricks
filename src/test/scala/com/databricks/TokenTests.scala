176ef3a7ab808eac42eb5a3e18ee9c70f6ff5327
Maxim Gekk <max.gekk@gmail.com>
2018-01-14 12:22:57 +0100

    Added a test for token list

--- src/test/scala/com/databricks/TokenTests.scala
+++ src/test/scala/com/databricks/TokenTests.scala
@@ -1,17 +1,22 @@
 package com.databricks
 
+import java.util.Calendar
+
 import com.typesafe.config.ConfigFactory
 import org.scalatest._
 
 class TokenTests extends FlatSpec with Matchers with BeforeAndAfter {
   var shard: ShardClient = _
+  val lifeTimeInSec: Long = 5*60
+  val now = Calendar.getInstance().getTime().toString
 
   before {
     shard = Shard(ConfigFactory.load("test-shard")).connect
   }
 
   it should "create new token and delete it" in {
-    val newToken = shard.token.create(5*60, "libricks testing")
+    val comment = "create test " + now
+    val newToken = shard.token.create(lifeTimeInSec, comment)
     shard.token.delete(newToken.token_info.token_id)
   }
 
@@ -19,5 +24,18 @@ class TokenTests extends FlatSpec with Matchers with BeforeAndAfter {
     intercept[ResourceDoesNotExists] {
       shard.token.delete("12345678")
     }
+  }
+
+  it should "lists created tokens" in {
+    val prefix = "list test"
+    val amount = 3
+    val tokens = for {
+      i <- 0 until amount
+      comment = s"$prefix $i $now"
+    } yield shard.token.create(lifeTimeInSec, comment)
+    val list = shard.token.list
+    val filteredTokens = list.filter(_.comment.startsWith(prefix))
+    assert(filteredTokens.size == amount)
+    tokens.foreach(t => shard.token.delete(t.token_info.token_id))
   }
 }
