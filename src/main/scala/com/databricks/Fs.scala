f1c81ec57246e319fe537855b49a0740ed0fe9aa
Maxim Gekk <maxim.gekk@databricks.com>
2018-03-12 13:28:07 +0100

    Implemented Fs.upload for small files (<1MB)

--- src/main/scala/com/databricks/Fs.scala
+++ src/main/scala/com/databricks/Fs.scala
@@ -1,13 +1,17 @@
 package com.databricks
 
-import java.io.File
+import java.io.{File}
+import java.nio.file.{Files}
 
 class Fs(client: ShardClient) extends Dbfs(client) {
 
-  def upload(src: String, dst: String): Unit = {
+  def upload(src: String, dst: String, overwrite: Boolean = true): Unit = {
     val localFile = new File(src)
     if (localFile.length() <= 1024*1024) {
-      ???
+      val data = Files.readAllBytes(localFile.toPath)
+      val block = WriteBlock(data)
+
+      put(dst, block, overwrite)
     } else {
       throw new NotImplementedError("Uploading file > 1MB is not supported")
     }
