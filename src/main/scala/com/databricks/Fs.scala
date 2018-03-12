d59701192deffbee037327097b8085ebd9b39ad1
Maxim Gekk <maxim.gekk@databricks.com>
2018-03-12 14:03:26 +0100

    Refactoring: assign name for 1MB

--- src/main/scala/com/databricks/Fs.scala
+++ src/main/scala/com/databricks/Fs.scala
@@ -5,9 +5,11 @@ import java.nio.file.{Files}
 
 class Fs(client: ShardClient) extends Dbfs(client) {
 
+  val MAX_BLOCK_SIZE = 1024*1024
+
   def upload(src: String, dst: String, overwrite: Boolean = true): Unit = {
     val localFile = new File(src)
-    if (localFile.length() <= 1024*1024) {
+    if (localFile.length() <= MAX_BLOCK_SIZE) {
       val data = Files.readAllBytes(localFile.toPath)
       val block = WriteBlock(data)
 
